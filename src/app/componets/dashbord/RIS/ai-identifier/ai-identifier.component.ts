0e3c9cee6cda5a49ff5db822e797e36022d732fd
planeteye-farm-ai <planetedevm@gmail.com>
2025-11-19 16:49:00 +0530

    link update

--- src/app/componets/dashbord/RIS/ai-identifier/ai-identifier.component.ts
+++ src/app/componets/dashbord/RIS/ai-identifier/ai-identifier.component.ts
@@ -7,14 +7,14 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './ai-identifier.component.html',
-  styleUrl: './ai-identifier.component.scss'
+  styleUrl: './ai-identifier.component.scss',
 })
 export class AiIdentifierComponent {
   aiDistressUrl: SafeResourceUrl;
-  
+
   constructor(private sanitizer: DomSanitizer) {
-    const url = 'https://ai-road-distress-detection-1011198718454.us-west1.run.app';
+    const url =
+      'https://main-ai-road-distress-detection-1011198718454.us-west1.run.app';
     this.aiDistressUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
-
